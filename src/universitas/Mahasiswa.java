767ccd10e92c22ba77f75a6f9cdd6692093de1a4
secnoz <anshari.3101@gmail.com>
2023-06-13 20:05:24 +0800

    Melengkapi class Mahasiswa

--- src/universitas/Mahasiswa.java
+++ src/universitas/Mahasiswa.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package universitas;
 
-/**
- *
- * @author ASUS
- */
 public class Mahasiswa {
+    String NPM;
+    String Nama;
+    String Prodi;
     
+    public Mahasiswa(){
+        this.NPM = "2110010449";
+        this.Nama = "Muhammad Anshari";
+        this.Prodi = "TI";
+    }
+    
+    public String getDetail(){
+       return this.NPM;
+    }
+    
+    public String getMhs(){
+        return this.Nama;
+    }
 }
